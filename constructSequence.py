5e0748a5aa02c4911f911b1bb62447d4042200ec
arafasse <olivia.waring@gmail.com>
2019-04-30 18:02:23 -0400

    Add ability to read in a sequence of phonemes and form a list of objects

---
+++ constructSequence.py
@@ -0,0 +1,11 @@
+import phonemicInventory
+
+with open('rigveda') as f:
+    lines = f.readlines()
+
+sequence = []
+for l in lines:
+	sequence.append(phonemicInventory.phonemeInv[l.strip()])
+
+#for s in sequence:
+#	s.printPhoneme()

8d4924733c8e9fbacdbbed065103e1296f4d0132
arafasse <olivia.waring@gmail.com>
2019-05-01 16:26:42 -0400

    Actually the class idea was bad, so here it is but we'll get rid of it

--- sandhi.py
+++ sandhi.py
@@ -14,11 +14,38 @@ print(sequence[2].equalsDifferentVowel(sequence[1]))
 
 phonemeInv = phonemicInventory.phonemeInv
 
+# no I actually don't want a class because I don't need any instances of rules...
+# I just need a probability dictionary, like I was thinking before... 
+# but I would like to have the data structures coincide somehow... 
+
+class Rule:
+    def __init__(self, prob):
+        self._prob = prob
+    
+    def get_prob(self):
+        return self._prob
+     
+    def set_prob(self, prob):
+        self._prob = prob
+        
+    def equals(self, other):
+        if self.vowel != other.get_vowel():
+            return False
+        return (self.orth == other.get_orth() and self.vowel == other.get_vowel() and 
+                self.PoA == other.get_PoA())
+    
+    prob = property(get_prob, set_prob)
+    
+    #def printPhoneme(self):
+    #    print(self._orth)
+
+
 # Define the probability dictionary
 RULES = {}
 RULES['C1'] = 0.2 # lower because of potential word boundaries 
 RULES['D1'] = 0.5
 RULES['D2'] = 0.5
+
 
 
 # Vowel rules (excerpted) 

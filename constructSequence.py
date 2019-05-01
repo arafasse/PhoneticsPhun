52ade4b79d83c88e9e78d33cdcacaa298bb3c3c5
arafasse <olivia.waring@gmail.com>
2019-04-30 20:38:00 -0400

    Fix isDifferentVowel function so it doesn't break on consonants, and write a quick and dirty version of the probabalistic apply function

--- constructSequence.py
+++ constructSequence.py
@@ -1,7 +1,11 @@
 import phonemicInventory
+import sandhi
+import random
 
 with open('rigveda') as f:
     lines = f.readlines()
+
+phonemeInv = phonemicInventory.phonemeInv
 
 sequence = []
 for l in lines:
@@ -10,3 +14,24 @@ for l in lines:
 for s in sequence:
 	#s.printPhoneme()
 	print(s.vowel)
+
+def D1(i):
+    if i >= len(sequence) - 1 or i < 0:
+        return
+    if not sequence[i].vowel or not sequence[i+1].vowel:
+    	return
+    if (sequence[i].equals(phonemeInv["i"]) or sequence[i].equals(phonemeInv["ii"])) and sequence[i+1].equalsDifferentVowel(sequence[i]):
+        sequence[i] = phonemeInv["y"]
+        return 
+
+def apply(rule, prob):
+	for i in range(0,len(sequence)):
+		randNum = random.random()
+		if randNum < prob:
+			rule(i)
+
+apply(D1,0.5)
+
+
+for s in sequence:
+	print(s.orth)
